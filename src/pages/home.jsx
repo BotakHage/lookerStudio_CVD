1b9ef2fdf9d859fa0f91dfbfd2857ef0da430fce
BotakHage <ahmadfauzia510@gmail.com>
2025-11-28 07:44:57 +0700

    test2

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,13 +5,14 @@ export default function Home() {
       <p>Your data, visualized.</p>
 
       <iframe
-        width="600"
-        height="450"
+        width={600}
+        height={450}
         src="https://lookerstudio.google.com/embed/reporting/2070b5d9-cb8f-427a-9d59-01e73e75c1ae/page/U75gF"
-        frameborder="0"
-        style="border:0"
-        allowfullscreen
-        sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+        frameBorder="0"
+        style={{ border: '0' }}
+        allowFullScreen
+        sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
+      />
     </div>
   );
 }
