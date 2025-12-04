fd23a3e0047c0240161af1334ffb52f845051c18
BotakHage <ahmadfauzia510@gmail.com>
2025-12-04 19:48:42 +0700

    update looker with data pegadaian

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,14 +5,13 @@ export default function Home() {
       <p>Your data, visualized.</p>
 
       <iframe
-        width={600}
-        height={450}
-        src="https://lookerstudio.google.com/embed/reporting/2070b5d9-cb8f-427a-9d59-01e73e75c1ae/page/U75gF"
-        frameBorder="0"
-        style={{ border: '0' }}
-        allowFullScreen
-        sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
-      />
+        width="1200"
+        height="1100"
+        src="https://lookerstudio.google.com/embed/reporting/326a3848-55d1-4e98-b47e-4f59b6e83081/page/FsJhF"
+        frameborder="0"
+        style="border:0"
+        allowfullscreen
+        sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
     </div>
   );
 }
