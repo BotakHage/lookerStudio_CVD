213c50eb7f6f380a4f04a8579a52e518a2e038e8
BotakHage <ahmadfauzia510@gmail.com>
2025-12-04 20:11:32 +0700

    done pgf

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,14 @@
 export default function Home() {
   return (
     <div>
-      <h1>Welcome</h1>
-
       <iframe
-        width="1200"
-        height="1100"
+        width={1200}
+        height={1100}
         src="https://lookerstudio.google.com/embed/reporting/326a3848-55d1-4e98-b47e-4f59b6e83081/page/FsJhF"
-        frameborder="0"
-        style="border:0"
-        allowfullscreen
-        sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+        style={{ border: 0 }}
+        allowFullScreen
+        sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
+      ></iframe>
     </div>
   );
 }
